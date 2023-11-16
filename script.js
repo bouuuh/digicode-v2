c5fa87198e5ea5d940bf9c95340c644babb487b3
b000uh <candice.doise@gmail.com>
2023-11-16 10:32:25 +0100

    new

--- script.js
+++ script.js
@@ -1,17 +1,66 @@
+// let boxes = document.querySelectorAll('.box');
+// let result = document.querySelector('.result');
+
+// function generateRandom() {
+//     let randomNumbers = [];
+//     let nbRandom;
+//     for (let i = 0; randomNumbers.length < 9; i++) {
+//         nbRandom = Math.floor((Math.random() * (9 - 1 + 1)) + 1);
+//         if (!randomNumbers.includes(nbRandom)) {
+//              randomNumbers.push(nbRandom);        
+//         } 
+//     }
+//         for (let b = 0; b < boxes.length; b++) {
+//             boxes[b].innerHTML = randomNumbers[b];  
+//         }
+//     }
+
+// generateRandom();
+
+// for (let i = 0; i < boxes.length; i++) {
+//     boxes[i].addEventListener('click', function getNumbers() {
+//         result.innerHTML += boxes[i].innerHTML;
+//         if (result.innerHTML === '1234') {
+//             result.style.background = 'green';
+//         }
+//         else if (result.innerHTML != '1234' && result.innerHTML.length === 4) {
+//             result.style.background = 'red';
+//         }
+//         else if(result.innerHTML.length === 5){
+//             result.innerHTML = '';
+//             result.style.background = "#593f62"
+//         }
+//         generateRandom();
+//     }) 
+       
+// }
+
+
+
+
+let tableau;
+    let numberTab;
+    let nbRandom;
+// Fonction de mélange aléatoire
+function melangerTableau() {
+    tableau = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    numberTab = [];
+    for (let i = 0; i < 9; i++) {
+        nbRandom = Math.floor(Math.random() * tableau.length);
+        numberTab.push(tableau[nbRandom]);
+        tableau.splice(nbRandom, 1);
+       
+    }
+   
+}
+
 let boxes = document.querySelectorAll('.box');
 let result = document.querySelector('.result');
 
 function generateRandom() {
-    let randomNumbers = [];
-    let nbRandom;
-    for (let i = 0; randomNumbers.length < 9; i++) {
-        nbRandom = Math.floor((Math.random() * (9 - 1 + 1)) + 1);
-        if (!randomNumbers.includes(nbRandom)) {
-             randomNumbers.push(nbRandom);        
-        } 
-    }
+        melangerTableau()
         for (let b = 0; b < boxes.length; b++) {
-            boxes[b].innerHTML = randomNumbers[b];  
+            boxes[b].innerHTML = numberTab[b];  
         }
     }
 
@@ -34,11 +83,3 @@ for (let i = 0; i < boxes.length; i++) {
     }) 
        
 }
-
-
-//étape 1 : écrire avec un clavier statique dans result
-//étape 2 : fond en vert si bon code et rouge sinon
-//étape 3 : fonction générer un nombre aléatoire entre 1 et 9
-//étape 4 : générer 9 nombres aléatoires différents et les stocker quelque part
-//étape 5 : transposer les nombres aléatoires dans le HTML
-//étape 6 : faire en sorte que lorsque j'appuie sur un bouton j'appelle à nouveau la fonction qi va les mélanger aléatoirement
